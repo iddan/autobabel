b723d4c1f4b35ca481a6faaba3fa17f5270933d3
YuanHao <shouhei.hayashi@gmail.com>
2016-08-11 10:06:41 +0800

    fix bug

--- main.js
+++ main.js
@@ -20,7 +20,9 @@ define(function (require, exports, module) {
 					'utf8'
 				);
 			} catch (e) {
-				brackets.getModule('widgets/Dialogs').showModalDialog(DefaultDialogs.DIALOG_ID_ERROR, "", "Unable to transform ES6 and JSX files into ES5, see the debug logs for details.");
+				var DefaultDialogs = brackets.getModule('widgets/DefaultDialogs');
+				var Dialogs = brackets.getModule('widgets/Dialogs');
+				Dialogs.showModalDialog(DefaultDialogs.DIALOG_ID_ERROR, "", "Unable to transform ES6 and JSX files into ES5, see the debug logs for details.");
 			}
 			break;
 		}
